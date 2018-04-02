918ea37c5818d4ea1a5e8466b89fbdebcbea7924
WangQingye <329103586@qq.com>
2018-04-02 18:03:10 +0800

    connect db

---
+++ server/database/init.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const db = 'mongodb://localhost/shunxin-goods'
+
+mongoose.Promise = global.Promise
+
+exports.connect = () => {
+    let maxConnectTimes = 0
+    return new Promise((resolve, reject) => {
+        if (process.env.NODE_ENV !== 'production') {
+            mongoose.set('debug', true)
+        }
+        mongoose.connect(db);
+        mongoose.connection.on('disconnected', () => {
+            if (maxConnectTimes < 5) {
+                mongoose.connect(db);
+            } else {
+                throw new Error('数据库挂了')
+            }
+        })
+        mongoose.connection.once('open', () => {
+            console.log('Mongodb Connected successfully')
+        })
+        mongoose.connection.on('error', err => {
+            console.log(err);
+        })
+    })
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const Koa = require('koa')
+const {
+    connect
+} = require('./database/init');
+(async () => {
+    await connect()
+})()
 const app = new Koa()
 
 app.use(async (ctx, next) => {
-    console.log(ctx.href);
-    console.log(ctx.path);
     ctx.body = 'Hi Luke'
 })
 
